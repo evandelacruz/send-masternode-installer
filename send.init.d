9e930491cf3518b2c5450cf667b4b2e7e3a7cf12
Evan de la Cruz <evandelacruz@users.noreply.github.com>
2017-12-09 16:54:30 -0700

    added the nodename to the description

--- send.init.d
+++ send.init.d
@@ -14,7 +14,7 @@
 
 # PATH should only include /usr/* if it runs after the mountnfs.sh script
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
-DESC="SEND masternode service"
+DESC="SEND masternode (instance: nodenameplaceholder) service"
 NODENAME=nodenameplaceholder
 NAME=send.$NODENAME
 DAEMON=/usr/sbin/sendd
